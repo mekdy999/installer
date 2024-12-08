5ff66409d251ee7e4347f8acb5fe4840d7151ca4
mekdy999 <mekdy3211@gmail.com>
2024-12-08 12:22:22 +0700

    Update installv5.sh

--- installv5.sh
+++ installv5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Tentukan lisensi yang valid
-VALID_LICENSE="Barkece"
+VALID_LICENSE="PutzTzy"
 
 # Path file lisensi dan file kesalahan
 LICENSE_FILE="/var/www/pterodactyl/license.txt"
